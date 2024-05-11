9d235aa86288b49ba053c0ad25cfb943a8124394
Tanmay Sharma <androtanmay43@gmail.com>
2024-05-11 18:34:44 +0530

    Moved components based processing to component itself

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,8 @@ import "./App.css";
 import Follower from "./components/Follower";
 
 function App() {
-  const emojiValues ={
-    "smiling":"/public/smiling.webp",
-    "whoozy":"/public/whoozy.webp"
-  }
   const [posX, setPosX] = useState(0);
   const [posY, setPosy] = useState(0);
-  const [emoji, setEmoji] = useState(emojiValues["smiling"]);
-
-  let lastPosX = 0;
-  let lastPosY = 0;
-  let isTired = false;
 
   //Add Event listener in window object to catch cursor movements
   window.addEventListener(
@@ -24,25 +15,10 @@ function App() {
         (e) => {
           let x = e.clientX; // Get X position of client cursor
           let y = e.clientY; // Get Y position of client cursor
-          let distance = Math.sqrt(
-            Math.pow(x - lastPosX, 2) + Math.pow(y - lastPosY, 2) // sqrt((x1-x2) + (y2-y1)) gets us the distance
-          ); // Get distance between last position and current postion (This gets us the speed by which the user moved the cursor)
-          lastPosX = x;
-          lastPosY = y;
-          if (distance > 100) { // If distance passes a certain value change the emoji icon state
-            setEmoji(emojiValues["whoozy"]);
-            isTired = true;
-            if (isTired) {
-              setTimeout(() => {
-                setEmoji(emojiValues["smiling"]);
-              }, 4000); // reset emoji back after 4 seconds ~ 4000ms
-              isTired = false;
-            }
-          }
-          setPosX(x);
-          setPosy(y);
+          setPosX(x); //pass the x coordinate to component
+          setPosy(y); //pass the x coordinate to component
         },
-        10,
+        20,
         { trailing: true, leading: true }
       ),
       [setPosX, setPosy]
@@ -51,9 +27,9 @@ function App() {
 
   return (
     <div>
-      <Follower posx={posX - 50} posy={posY - 50} emoji={emoji}></Follower>
+      <Follower posx={posX} posy={posY}></Follower>
     </div>
-  ); // Wer subtract 50 from the actual position to keep the cursor centered , since the circular div have 100 diameter and we need subtract 50 to keep it centeralized
+  );
 }
 
 export default App;
--- src/components/Follower.jsx
+++ src/components/Follower.jsx
@@ -1,13 +1,51 @@
-import React from 'react'
-import '../App.css'
+import React, { useState } from "react";
+import "../App.css";
 
-function Follower({posx,posy,emoji}) {
+const emojiValues = {
+  smiling: "/public/smiling.webp",
+  whoozy: "/public/whoozy.webp",
+};
+let lastPosX = 0;
+let lastPosY = 0;
+
+function Follower({ posx, posy }) {
+  const [emoji, setEmoji] = useState(emojiValues["smiling"]);
+
+  function calculateState() {
+    let isTired = false;
+
+    let distance = Math.sqrt(
+      Math.pow(posx - lastPosX, 2) + Math.pow(posy - lastPosY, 2) // sqrt((x1-x2) + (y2-y1)) gets us the distance
+    ); // Get distance between last position and current postion (This gets us the speed by which the user moved the cursor)
+
+    console.log(distance);
+    lastPosX = posx;
+    lastPosY = posy;
+    if (distance > 100) {
+      // If distance passes a certain value change the emoji icon state
+      setEmoji(emojiValues["whoozy"]);
+      isTired = true;
+      if (isTired) {
+        setTimeout(() => {
+          setEmoji(emojiValues["smiling"]);
+        }, 4000); // reset emoji back after 4 seconds ~ 4000ms
+        isTired = false;
+      }
+    }
+  }
+
+  calculateState(posx, posy);
   return (
-    <div style={{left: posx+"px",top: posy+"px"}} className="follower">
+    <div
+      style={{ left: posx - 50 + "px", top: posy - 50 + "px" }} // We subtract 50 from the actual position to keep the cursor centered , since the circular div have 100 diameter and we need subtract 50 to keep it centeralized
+      className="follower"
+    >
       <div className="background"></div>
-      <p><img width="100px" src={emoji} alt="" /></p>
+      <p>
+        <img width="100px" src={emoji} alt="" /> 
+      </p>
     </div>
-  )
+  );
 }
 
-export default Follower
+export default Follower;
