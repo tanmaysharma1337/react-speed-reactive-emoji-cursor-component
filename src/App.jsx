5161ea5363aa339c555f1126b3557af6bea1b895
Tanmay Sharma <androtanmay43@gmail.com>
2024-05-11 14:32:43 +0530

    made the emoji cursor reactive (based on mouse cursor speed)

--- src/App.jsx
+++ src/App.jsx
@@ -4,26 +4,53 @@ import "./App.css";
 import Follower from "./components/Follower";
 
 function App() {
+  const emojiValues ={
+    "smiling":"/public/smiling.webp",
+    "whoozy":"/public/whoozy.webp"
+  }
   const [posX, setPosX] = useState(0);
   const [posY, setPosy] = useState(0);
+  const [emoji, setEmoji] = useState(emojiValues["smiling"]);
+
+  let lastPosX = 0;
+  let lastPosY = 0;
+  let isTired = false;
 
   window.addEventListener(
     "mousemove",
-    useCallback(throttle(
-      (e) => {
-        let x = e.clientX;
-        let y = e.clientY;
-        setPosX(x);
-        setPosy(y);
-      },
-      10,
-      { trailing: true, leading: true }
-    ),[setPosX,setPosy])
+    useCallback(
+      throttle(
+        (e) => {
+          let x = e.clientX;
+          let y = e.clientY;
+          let distance = Math.sqrt(
+            Math.pow(x - lastPosX, 2) + Math.pow(y - lastPosY, 2)
+          );
+          lastPosX = x;
+          lastPosY = y;
+          if (distance > 100) {
+            setEmoji(emojiValues["whoozy"]);
+            isTired = true;
+            if (isTired) {
+              setTimeout(() => {
+                setEmoji(emojiValues["smiling"]);
+              }, 4000);
+              isTired = false;
+            }
+          }
+          setPosX(x);
+          setPosy(y);
+        },
+        10,
+        { trailing: true, leading: true }
+      ),
+      [setPosX, setPosy]
+    )
   );
-  
+
   return (
     <div>
-      <Follower posx={posX - 50} posy={posY - 50}></Follower>
+      <Follower posx={posX - 50} posy={posY - 50} emoji={emoji}></Follower>
     </div>
   );
 }
--- src/components/Follower.jsx
+++ src/components/Follower.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import '../App.css'
 
-function Follower({posx,posy}) {
+function Follower({posx,posy,emoji}) {
   return (
     <div style={{left: posx+"px",top: posy+"px"}} className="follower">
       <div className="background"></div>
-      <p>🥴</p>
+      <p><img width="100px" src={emoji} alt="" /></p>
     </div>
   )
 }
